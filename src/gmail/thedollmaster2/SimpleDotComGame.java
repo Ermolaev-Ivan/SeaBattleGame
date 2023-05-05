9edf193e4fc8e4d6ab7fd29afeebd91bc501ee29
ErmolaevIvan <thedollmaster@bk.ru>
2023-05-05 20:48:13 +0300

    First commit.

---
+++ src/gmail/thedollmaster2/SimpleDotComGame.java
@@ -0,0 +1,28 @@
+package gmail.thedollmaster2;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class SimpleDotComGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SimpleDotCom dot = new SimpleDotCom();
+        String result = "";
+        int numOfGuesses = 0;
+        int firstPositionLocations = (int) (Math.random() * 4);
+        int[] locations = {firstPositionLocations, firstPositionLocations + 1, firstPositionLocations + 2};
+
+        dot.setLocationCells(locations);
+
+        while (!result.equals("Потопил")){
+
+            System.out.println("Введите число: ");
+            String userGuess = sc.nextLine();
+            result = dot.checkYourSelf(userGuess);
+            numOfGuesses ++;
+        }
+        System.out.println("Вы затратили "+ numOfGuesses + " хода(ов)!");
+
+    }
+}
---
+++ src/gmail/thedollmaster2/SimpleDotComTestDrive.java
@@ -0,0 +1,24 @@
+package gmail.thedollmaster2;
+
+public class SimpleDotComTestDrive {
+
+    public static void main(String[] args){
+
+        SimpleDotCom dot = new SimpleDotCom();
+        int[] locations = {2,3,4};
+        dot.setLocationCells(locations);
+        String userGuess = "2";
+        String result = dot.checkYourSelf(userGuess);
+
+//        String testResult = "Неудача";
+//
+//        if (result.equals("Попал")){
+//
+//            testResult = "Пройден";
+//
+//        }
+//
+//        System.out.println(testResult);
+
+    }
+}
